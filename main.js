8bda687d2e1585d692abdbc8043d29d74d491746
adarsh anchan <anchanadarsh@gmail.com>
2018-03-13 11:54:56 +0530

    fsaf

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 var bingo = angular.module("bingo", []);
 bingo.controller("bingoController", ['$scope', function ($scope) {
-    $scope.numArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25'];
-    $scope.numArr.sort(function(a, b){return 0.5 - Math.random()});
-    console.log($scope.numArr);
+    $scope.numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+    $scope.numArr.sort(function (a, b) {
+        return 0.5 - Math.random()
+    });
+    //    console.log($scope.numArr);
     $scope.guessArr = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''];
+    $scope.guessnum;
+    $scope.arrIndex;
+    $scope.goBingo = function () {
+        if ($scope.guessnum != '') {
+            $scope.arrIndex = $scope.numArr.indexOf($scope.guessnum);
+            //            console.log($scope.arrIndex);
+            $scope.guessArr[$scope.arrIndex] = $scope.guessnum;
+            $scope.guessnum = '';
+        }
+    }
 }]);
